c022aac177f85273b9f6e1650877b3fe4347a9ab
Tim McIver <tim@timmciver.com>
2012-10-02 22:54:56 -0400

    Updated API so that a game resource is created by sending a PO ST request to the '/game' URI and the game map is retrieved by sending a GET to a URI of the form 'http://localhost/game/123456'

--- src/crosscram_ws/server.clj
+++ src/crosscram_ws/server.clj
@@ -3,10 +3,12 @@
             [compojure.route :as route]
             [ring.adapter.jetty :as ring]
             [ring.middleware.params :as rmp]
+            [ring.util.response :as rur]
             [crosscram.engine :as engine]
             [crosscram.game :as game]
             [crosscram.main]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
 (defn- game
   "Returns a game played between bot-a and bot-b (bot fns) on a board with
@@ -16,19 +18,45 @@ dimensions dim-1 and dim-2."
         g (engine/play g (first botfns) (second botfns))]
     g))
 
-(defn- get-game
+(defn- post-game
+  "Runs a game of crosscram using the given post parameters and returns a 303
+  \"See Other\" response including a link to the game resource."
   [req]
-  (let [dims (if-let [dimstr ((:params req) "dims")]
-               (read-string dimstr)
-               [8 8])
-        botnames (if-let [botstr ((:params req) "bots")]
-                   (read-string botstr)
-                   ['crosscram.samples.random 'crosscram.samples.random])
-        botfns (map #(:make-move (#'crosscram.main/load-player %)) botnames)]
-    (json/json-str (game botfns dims))))
+  (let [params (:params req)
+        {:strs [dims bots]} params]
+    (if (or (nil? dims) (= 0 (count dims)) (nil? bots) (= 0 (count bots)))
+      {:status 400 :body "One or more parameters were missing."}
+      (let [dims (read-string dims)
+            bots (read-string bots)
+            botfns (map #(:make-move (#'crosscram.main/load-player %)) bots)
+            g (game botfns dims)
+            fname (str "games/"
+                       (->> (rand)
+                            (* 1e6)
+                            int
+                            str)
+                       ".clj")]
+        (with-open [wrtr (io/writer fname)]
+          (binding [*print-dup* true
+                    *out* wrtr]
+            (prn g)))
+        (rur/redirect-after-post "games/test-game.clj")))))
+
+(defn- get-game
+  "Returns the game map for the game whose ID is given in the :id parameter."
+  [req]
+  (let [gid ((:params req) :id)
+        fname (str "games/" gid ".clj")
+        f (java.io.File. fname)]
+    (if (.exists f)
+      (let [g (with-open [rdr (io/reader f)]
+                (read (java.io.PushbackReader. rdr)))]
+        (json/json-str g))
+      (rur/not-found "No such game."))))
 
 (comp/defroutes routes
-  (comp/GET "/game" [] get-game)
+  (comp/GET "/game/:id" [id] get-game)
+  (comp/POST "/game" [] post-game)
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app (-> routes
