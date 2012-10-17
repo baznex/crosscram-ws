18eafe91b2aae8ddb30e4d3411e112965200cf4a
Tim McIver <tim@timmciver.com>
2012-10-17 19:17:49 -0400

    Fixed html functions that use template function; were calling template function with incorrect number of args.

--- src/crosscram_ws/views/html.clj
+++ src/crosscram_ws/views/html.clj
@@ -17,6 +17,7 @@
         `[:html
           [:head
            [:title ~title]
+                                        ;           ~(page/include-css "/css/crosscram.css")
            ~@js-includes
            ~@links]
           [:body
@@ -30,39 +31,42 @@
   "Returns an HTML representaion of the 'start' resource."
   []
   (template "Welcome to Crosscram!"
-            [:h2 "Welcome to Crosscram!"]
-            [:ul
-             [:li (elem/link-to "/create-game" "Create a new game")]
-             [:li (elem/link-to "/games" "List of previously played games")]]))
+            [:div
+             [:h2 "Welcome to Crosscram!"]
+             [:ul
+              [:li (elem/link-to "/create-game" "Create a new game")]
+              [:li (elem/link-to "/games" "List of previously played games")]]]))
 
 (defn games-to-html
   "Returns an HTML representaion of the 'games' resource given a collection of IDs."
   [gameids]
   (template "List of Crosscram Games"
-            [:h2 "Played games:"]
-            [:ul (map (fn [fname]
-                        [:li (elem/link-to (str "game/" fname) fname)])
-                      gameids)]))
+            [:div
+             [:h2 "Played games:"]
+             [:ul (map (fn [fname]
+                         [:li (elem/link-to (str "game/" fname) fname)])
+                       gameids)]]))
 
 (defn create-game-to-html
   "Returns an HTML representaion of the 'create-game' resource."
   []
   (template "Create a game of Crosscram"
-            [:h2 "Create a Crosscram Game"]
-            (form/form-to [:post "/game"]
-                          [:div
-                           [:label "Number of rows:"]
-                           (form/text-field "rows")]
-                          [:div
-                           [:label "Number of columns:"]
-                           (form/text-field "cols")]
-                          [:div
-                           [:label "First bot:"]
-                           (form/text-field "bot1")]
-                          [:div
-                           [:label "Second bot:"]
-                           (form/text-field "bot2")]
-                          (form/submit-button "Create Game"))))
+            [:div
+             [:h2 "Create a Crosscram Game"]
+             (form/form-to [:post "/game"]
+                           [:div
+                            [:label "Number of rows:"]
+                            (form/text-field "rows")]
+                           [:div
+                            [:label "Number of columns:"]
+                            (form/text-field "cols")]
+                           [:div
+                            [:label "First bot:"]
+                            (form/text-field "bot1")]
+                           [:div
+                            [:label "Second bot:"]
+                            (form/text-field "bot2")]
+                           (form/submit-button "Create Game"))]))
 
 (defn game-to-html
   "Returns an HTML representaion of the 'game' resource."
