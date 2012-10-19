55b3be16f52c086bd0d18241d1c8a77623533755
Tim McIver <tim@timmciver.com>
2012-10-19 19:29:15 -0400

    Upgraded from clojure 1.3 to 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject crosscram-ws "1.0.0-SNAPSHOT"
   :description "A web interface for Crosscram"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]
                  [ring/ring-jetty-adapter "1.1.6"]
                  [ring/ring-core "1.1.6"]
